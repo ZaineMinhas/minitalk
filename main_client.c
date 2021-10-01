36425510f87e264b71bf89fd704be94c6b7a18c0
ZaineMinhas <zaineminhas5@gmail.com>
2021-10-01 17:16:10 +0200

    test en cours...

--- main_client.c
+++ main_client.c
@@ -6,7 +6,7 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 18:37:23 by zminhas           #+#    #+#             */
-/*   Updated: 2021/09/28 17:00:22 by zminhas          ###   ########.fr       */
+/*   Updated: 2021/10/01 16:41:52 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,12 +53,21 @@ static void	share_signal(char c, int pid)
 
 int	main(int argc, char **argv)
 {
-	int	pid;
 	int	i;
+	int	j;
 
 	check_error(argc, argv);
-	pid = ft_atoi(argv[1]);
 	i = -1;
+	ft_putstr("send bytes : [");
+	ft_putnbr(ft_strlen(argv[2]));
+	ft_putstr("]\n");
+	j = 0;
 	while (argv[2][++i])
-		share_signal(argv[2][i], pid);
+	{
+		share_signal(argv[2][i], ft_atoi(argv[1]));
+		j++;
+		ft_putstr("\rrecive bytes : [");
+		ft_putnbr(j);
+		ft_putstr("]");
+	}
 }
--- main_server.c
+++ main_server.c
@@ -6,18 +6,18 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 16:36:29 by zminhas           #+#    #+#             */
-/*   Updated: 2021/09/26 18:59:25 by zminhas          ###   ########.fr       */
+/*   Updated: 2021/10/01 16:42:03 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-t_sigchar	g_char;
+t_sigchar	*g_char;
 
 static void	init(void)
 {
-	g_char.c = 0;
-	g_char.bit = 0;
+	g_char->c = 0;
+	g_char->bit = 0;
 }
 
 static void	print_pid(void)
@@ -29,17 +29,18 @@ static void	print_pid(void)
 static void	intercom(int sig)
 {
 	if (sig == SIGUSR1)
-		g_char.c += ft_pow(2, g_char.bit);
-	g_char.bit++;
-	if (g_char.bit == 7)
+		g_char->c += ft_pow(2, g_char->bit);
+	g_char->bit++;
+	if (g_char->bit == 7)
 	{
-		ft_putchar(g_char.c);
+		ft_putchar(g_char->c);
 		init();
 	}
 }
 
 int	main(void)
 {
+	g_char = malloc(sizeof(t_sigchar));
 	init();
 	print_pid();
 	if (signal(SIGUSR1, intercom) == SIG_ERR
