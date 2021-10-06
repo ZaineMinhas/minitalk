2757e23c04fa15e8397699e5564ee21cf34708bb
ZaineMinhas <zaineminhas5@gmail.com>
2021-10-06 19:10:21 +0200

    ez fini

--- main_client.c
+++ main_client.c
@@ -6,25 +6,19 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 18:37:23 by zminhas           #+#    #+#             */
-/*   Updated: 2021/10/06 16:06:02 by zminhas          ###   ########.fr       */
+/*   Updated: 2021/10/06 19:06:05 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
 
 static void	check_error(int argc, char **argv)
 {
 	if (argc != 3)
-	{
 		ft_putstr("Only two parameters required\n");
-		exit(1);
-	}
 	if (ft_atoi(argv[1]) <= 0)
-	{
 		ft_putstr("The pid must be greater than 0\n");
-		exit(1);
-	}
+	exit(1);
 }
 
 static void	send(int pid, int signal)
@@ -36,7 +30,7 @@ static void	send(int pid, int signal)
 	}
 }
 
-static void	share_len(int len, int pid)
+static void	share_len(int len, int pid, int speed)
 {
 	int	i;
 
@@ -48,11 +42,11 @@ static void	share_len(int len, int pid)
 		else
 			send(pid, SIGUSR2);
 		len /= 2;
-		usleep(100);
+		usleep(speed);
 	}
 }
 
-static void	share_char(char c, int pid)
+static void	share_char(char c, int pid, int speed)
 {
 	int	i;
 
@@ -60,37 +54,23 @@ static void	share_char(char c, int pid)
 	while (++i < 8)
 	{
 		if (c % 2)
-		{
-			//printf("1");
 			send(pid, SIGUSR1);
-		}
 		else
-		{
-			//printf("0");
 			send(pid, SIGUSR2);
-		}
 		c /= 2;
-		usleep(100);
+		usleep(speed);
 	}
-	//printf("\n");
 }
 
 int	main(int argc, char **argv)
 {
 	int	i;
-	int	j;
 
 	check_error(argc, argv);
-	share_len(ft_strlen(argv[2]), ft_atoi(argv[1]));
+	share_len(ft_strlen(argv[2]), ft_atoi(argv[1]), 50);
 	i = -1;
-	j = ft_strlen(argv[2]);
+	ft_putnbr(ft_strlen(argv[2]));
 	while (argv[2][++i])
-	{
-		share_char(argv[2][i], ft_atoi(argv[1]));
-		ft_putstr("\rbytes received : [");
-		ft_putnbr(i + 1);
-		ft_putstr(" / ");
-		ft_putnbr(j);
-		ft_putstr("]");
-	}
+		share_char(argv[2][i], ft_atoi(argv[1]), 50);
+	return (0);
 }
--- main_server.c
+++ main_server.c
@@ -6,30 +6,43 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 16:36:29 by zminhas           #+#    #+#             */
-/*   Updated: 2021/10/06 16:09:29 by zminhas          ###   ########.fr       */
+/*   Updated: 2021/10/06 18:50:08 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
 
 t_sigchar	g_char;
+
+static void	return_error(int index)
+{
+	if (!index)
+		ft_putstr("Error\n");
+	else if (index == 1)
+		ft_putstr("Malloc error\n");
+	else if (index == 2)
+		ft_putstr("Write error\n");
+	exit(1);
+}
 
 static void	init(void)
 {
 	g_char.c = 0;
 	g_char.bit = -1;
+	g_char.i = 0;
+	g_char.len = 0;
+	g_char.str = NULL;
 }
 
 static void	len_calc(int sig)
 {
 	if (sig == SIGUSR1)
-			g_char.len += ft_pow(2, g_char.bit);
+		g_char.len += ft_pow(2, g_char.bit);
 	if (g_char.bit == 31)
 	{
 		g_char.str = malloc(sizeof(char) * (g_char.len + 1));
 		if (!g_char.str)
-			exit(1);
+			return_error(1);
 		g_char.bit = -1;
 	}
 }
@@ -42,28 +55,20 @@ static void	intercom(int sig)
 	else
 	{
 		if (sig == SIGUSR1)
-		{
-			//printf("1 char.c = %d + 2^%d == %d\n", g_char.c, g_char.bit, g_char.c + ft_pow(2, g_char.bit));
 			g_char.c += ft_pow(2, g_char.bit);
-		}
-		else
-			//printf("0 char.c = %d && bit = %d\n", g_char.c, g_char.bit);
 		if (g_char.bit == 7)
 		{
-			//printf("int char = %d\nchar = %c\n", g_char.c, g_char.c);
-			//printf("\n");
 			g_char.str[g_char.i++] = g_char.c;
-			init();
+			g_char.c = 0;
+			g_char.bit = -1;
 		}
 	}
 	if (g_char.i == g_char.len && g_char.len)
 	{
 		g_char.str[g_char.i + 1] = 0;
-		ft_putstr(g_char.str);
+		if (write(1, g_char.str, g_char.len) == -1)
+			return_error(2);
 		free(g_char.str);
-		g_char.str = NULL;
-		g_char.len = 0;
-		g_char.i = 0;
 		init();
 	}
 }
@@ -71,9 +76,6 @@ static void	intercom(int sig)
 int	main(void)
 {
 	init();
-	g_char.i = 0;
-	g_char.len = 0;
-	g_char.str = NULL;
 	ft_putnbr(getpid());
 	ft_putchar('\n');
 	if (signal(SIGUSR1, intercom) == SIG_ERR
